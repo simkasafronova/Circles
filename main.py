0da58f31b5955a105f294bc77e34d12c6a151221
Sima Safronova <serafima.safronowa@gmail.com>
2020-11-27 15:45:29 +0300

    different colors without uic

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
 
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from random import randrange, choice
+from UI import Ui_MainWindow
 
 COLORS = [(168, 54, 50), (232, 129, 44), (232, 210, 44), (100, 191, 48),
           (34, 138, 52), (39, 171, 160), (34, 78, 161), (92, 35, 158)]
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.start_drawing)
         self.paint_flag = False
 
